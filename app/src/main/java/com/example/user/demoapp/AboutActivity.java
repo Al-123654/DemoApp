5a15518ca6b28ad68a4c3bc694ac5c5414195325
Al-123654 <alif.numan@sphiere.com.bn>
2018-07-09 17:05:08 +0800

    3rd commit

--- app/src/main/java/com/example/user/demoapp/AboutActivity.java
+++ app/src/main/java/com/example/user/demoapp/AboutActivity.java
@@ -2,6 +2,10 @@ package com.example.user.demoapp;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutActivity extends FragmentActivity {
 
@@ -9,5 +13,12 @@ public class AboutActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        WebView myWebView = findViewById(R.id.about_webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        myWebView.loadUrl("http://www.asean.org");
     }
 }
--- app/src/main/java/com/example/user/demoapp/HomeActivity.java
+++ app/src/main/java/com/example/user/demoapp/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -20,6 +21,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.home_toolbar);
+        setSupportActionBar(myToolbar);
 
         mAboutButton = findViewById(R.id.home_left_button);
         mAboutButton.setOnClickListener(new View.OnClickListener() {
